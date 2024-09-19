3a51a4363b07a5621df999f1006ca9f67b4af326
bharat <bharatpanigrahi225@gmail.com>
2024-09-20 02:19:48 +0530

    fix: all issue

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
-// export const baseURL = "https://mess-review-backend.onrender.com"
-export const baseURL = "http://localhost:9000"
+export const baseURL = "https://mess-review-backend.onrender.com"
+// export const baseURL = "http://localhost:9000"
